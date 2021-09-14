f7d7346bd46e0079b1c239d87b9cbd138bd2e326
aashishj22 <89920856+aashishj22@users.noreply.github.com>
2021-09-14 11:36:28 +0530

    timepass
    nodoubt

---
+++ Homework/14sep21/if.php
@@ -0,0 +1,137 @@
+<?php
+
+
+// ----------------leap year----------
+// $year=2016 ;
+// if ($year%4==0) {
+// 	echo "leap year";
+// }
+// else{
+// 	echo"not";
+// }
+
+// -------------------------------------------------
+		// profit and loss
+
+// $costPrice=1330;
+// $sellingPrice=14000;
+
+
+// if ($sellingPrice==$costPrice) {
+// 	echo "no prof no loss";
+// }
+// elseif ($sellingPrice>$costPrice) {
+// 	echo"profit";
+// }
+// else{
+// 	echo"loss";
+// }
+// ------------------------------------------------------------------------
+
+			// swapping numbers
+
+
+// $a = 20;
+// $b = 21;
+
+// // swapping logic
+// $c= $a;
+// $a=$b;
+// $b=$c;
+// echo "after swapping:<br>";
+// echo"a=".$a. "b=".$b;
+// -------------------------------------------------
+
+
+// $i=0;
+// while ($i<10) {
+// 	$i++;
+// 	$a = $i * 2;
+	
+// 	echo (" 2 x ".$i."=".$a."<br>");
+// 	// echo ($i."AASHISH"."<br>");
+// }
+
+// ----------------------------------------------------------
+// $i=0;
+// while ($i<10) {
+// 	$i++;
+// 	$a = $i+2;
+	
+// 	echo (" 2 x ".$i."=".$a."<br>");
+// 	// echo ($i."AASHISH"."<br>");
+// }
+
+
+
+// -----------------------------------------------------
+
+
+for ($i=0; $i<10 ; $i++) { 
+	if ($i%2==0) {
+		echo "the number is even".$i."<br>";
+	}else {
+		echo"odd".$i."<br>";
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if ($a>0) {
+// 	echo "the number is positive";
+// }elseif ($a<0) {
+// 	echo "the number is negative"; 
+// }else{
+// 	echo"the numbber is zero";
+// }
+
+
+
+
+
+
+
+
+
+
+// if($var>15){
+// 	echo "age is greater";
+
+// }else{
+// 	echo "age is not greater ";
+// }
+
+
+
+
+
+
+
+
+?>
