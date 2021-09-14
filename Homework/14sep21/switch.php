78e5013812f4bb16726806c8f62f7b9cb69b09fb
aashishj22 <89920856+aashishj22@users.noreply.github.com>
2021-09-14 18:59:26 +0530

    switch wala sir

---
+++ Homework/14sep21/switch.php
@@ -0,0 +1,46 @@
+<?php
+
+$day="sun";
+switch ($day) {
+	case 'mon':
+		echo "The day is Monday";
+		break;
+
+		case 'tue':
+		echo "the day is Tuesday";
+		break;
+
+		case 'wed':
+		echo "the day is Wednesday";
+		break;
+
+		case 'thurs':
+		echo "the day is Thursday";
+		break;
+		
+
+		case 'fri':
+		echo "the day is Friday";
+		break;
+		
+		case 'sat':
+		echo "the day is Saturday";
+		break;
+	
+		case 'sun':
+		echo"the day is Sunday";
+		break;
+	
+	
+	
+	default:
+		echo "Please enter valid day";
+		break;
+}
+
+
+
+
+
+
+?>
